1bf9107a2ea6318b36f542725040ba02a5e7e822
sahar <saharabumuaileq12@gmail.com>
2021-11-18 13:25:38 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,115 @@
+$(document).ready(function(){
+ $('.productSection div').click(function(){
+     $('.classPart').toggle();
+ });
+
+ $('.classPart a.diary').click(function(){
+     $('.showDetails').html($('.diaryDetails').html()).css({'color':'#402F27'});
+ });
+ $('.classPart a.drinks').click(function(){
+     $('.showDetails').html($('.drinksDetails').html()).css({'color':'#402F27'});
+ });
+//  .css({'color':'#E4BE96'})
+
+
+
+
+$(".twsel").hide();
+$(".push").click(function(){
+    $(".twsel").show().siblings(".a").hide();
+
+
+} );
+$(".twsel1").hide();
+$(".del").click(function(){
+    $(".twsel1").show().siblings(".a").hide();
+
+
+} );
+$(".twsel2").hide();
+$(".qustion").click(function(){
+    $(".twsel2").show().siblings(".a").hide();
+
+
+} );
+$(".twsel3").hide();
+$(".tel").click(function(){
+    $(".twsel3").show().siblings(".a").hide();
+
+
+} );
+$(".we").hide();
+$(".qul").click(function(){
+    $(".we").show().siblings(".a").hide();
+
+
+} );
+$(".we1").hide();
+$(".gift").click(function(){
+    $(".we1").show().siblings(".a").hide();
+
+
+} );
+$(".we2").hide();
+$(".gift").click(function(){
+    $(".we2").show().siblings(".a").hide();
+
+
+} );
+
+
+$(".offer").hide();
+$(".laban").click(function(){
+    $(".offer").toggle().siblings(".d").hide();
+
+
+});
+$(".offe").hide();
+$(".buscit").click(function(){
+    $(".offe").toggle().siblings(".d").hide();
+
+
+});
+$(".labn").hide();
+$(".boog").click(function(){
+    $(".labn").toggle().siblings(".d").hide();
+
+
+});
+$(".electron").hide();
+$(".electronic").click(function(){
+    $(".electron").toggle().siblings(".d").hide();
+
+
+});
+$(".all").hide();
+$(".al").click(function(){
+    $(".all").toggle().siblings(".d").hide();
+
+
+});
+$(".cllean").hide();
+$(".cleans").click(function(){
+    $(".cllean").toggle().siblings(".d").hide();
+
+
+});
+$(".offers .baskot").hide();
+$(".drinks").click(function(){
+    $(".offers .baskot").toggle().siblings(".d").hide();
+
+
+});
+$(".offers .bb").hide();
+$(".cons").click(function(){
+    $(".offers .bb").toggle().siblings(".d").hide();
+
+
+});
+$(".offers .bb").hide();
+$(".buscit").click(function(){
+    $(".offers .bb").toggle().siblings(".d").hide();
+
+
+});
+});
